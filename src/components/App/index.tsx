36adea46b6648a7e7d3fb6315881377e1b56511f
Yumi Patton <yumipatton@gmail.com>
2021-08-22 16:58:35 +1000

    fnished generate cells logic

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
     const [cells, setCells] = useState(generateCells());
     console.log("cells", cells);
     const renderCells = (): React.ReactNode => {
-        return cells.map((row, rowIndex) => row.map((cell, colIndex) => <Button key={`${rowIndex}-${colIndex}`} />));
+        return cells.map((row, rowIndex) => row.map((cell, colIndex) => <Button key={`${rowIndex}-${colIndex}`} state={cell.state} value={cell.value} row={ rowIndex } col={ colIndex }/>));
     }
     return <div className="App" >
         <div className="Header">
